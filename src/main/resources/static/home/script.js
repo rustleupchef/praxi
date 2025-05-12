7859308cc43617cbd5a733cda696690ac73431b0
rustleupchef <rustle.up.chef@gmail.com>
2025-05-12 17:53:17 -0500

    added readystate checking

--- src/main/resources/static/home/script.js
+++ src/main/resources/static/home/script.js
@@ -4,7 +4,7 @@ function submit() {
     const model = encodeURIComponent(document.getElementById("model").value);
     xhr.open("POST", "/submit?prompt=" + prompt + "&model=" + model, true);
     xhr.onload = function () {
-        if (xhr.status === 200) {
+        if (xhr.status === 200 && xhr.readyState === 4) {
             const response = JSON.parse(xhr.responseText);
             if (response.type === "error") {
                 console.error("Error:", response.message);
