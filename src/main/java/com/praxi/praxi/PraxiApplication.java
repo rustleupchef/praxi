caeec882f297e8d0495b24b21fe99654bff45129
rustleupchef <rustle.up.chef@gmail.com>
2025-06-13 09:10:11 -0500

    automatically creates ip file if it doesn't exist

--- src/main/java/com/praxi/praxi/PraxiApplication.java
+++ src/main/java/com/praxi/praxi/PraxiApplication.java
@@ -1,12 +1,22 @@
 package com.praxi.praxi;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PraxiApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		if (!new File("ip").exists()) {
+            FileWriter writer = new FileWriter(new File("ip"));
+			writer.write("");
+			writer.close();
+			return;
+        }
 		SpringApplication.run(PraxiApplication.class, args);
 	}
 
